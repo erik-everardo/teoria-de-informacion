ba49dee1b25af3fee583b028239a58a0afd2e9f6
Erik Everardo <erik10cavazos@gmail.com>
2022-05-29 23:33:51 -0500

    Add files via upload

---
+++ Huffman/Nodo.cs
@@ -0,0 +1,47 @@
+﻿namespace Huffman;
+
+
+public class Node
+{
+    // la clave es la concatenacion de los caracteres y el valor es el peso de todos juntos
+    public Dictionary<string, int> Valor { get; set; }
+    public Node? Padre { get; set; }
+    public Node? HijoIzquierdo { get; set; }
+    public Node? HijoDerecho { get; set; }
+
+    public override string ToString()
+    {
+        return $" \n{Valor.First().Key}  {HijoIzquierdo?.Valor.First().Key ?? ""}  {HijoDerecho?.Valor.First().Key ?? ""}";
+    }
+    
+    public int NodeWeight()
+    {
+        var peso = 0;
+        foreach (var (k,v) in Valor)
+        {
+            peso += v;
+        }
+
+        return peso;
+    }
+
+    public void PrintRecursively()
+    {
+        Console.Write(ToString());
+        HijoDerecho?.PrintRecursively();
+        HijoIzquierdo?.PrintRecursively();
+    }
+
+    public void Encode(char character)
+    {
+        var accumulated = "";
+        if (Padre != null)
+        {
+            Padre.Encode(character);
+        }
+
+        Console.Write(HijoDerecho?.Valor.First().Key.Contains(character) == true ? "1" : "0");
+        
+
+    }
+}
---
+++ Huffman/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using Huffman;
+
+Dictionary<string,int> SymbolsOccurrence(string text, bool spaces)
+{
+    var characters = new Dictionary<string, int>();
+
+    foreach (var character in text)
+    {
+        if (characters.ContainsKey(character.ToString()))
+        {
+            characters[character.ToString()] += 1;
+        }
+        else
+        {
+            characters.Add(character.ToString(),1);
+        }
+    }
+
+    if (spaces) return characters;
+    
+    if (characters.ContainsKey(" "))
+    {
+        characters.Remove(" ");
+    }
+
+    return characters;
+}
+
+Dictionary<char, double> Frequencies(Dictionary<char, int> occurrences, int numberOfCharacters)
+{
+    var frequencies = new Dictionary<char, double>();
+    foreach (var (character, times) in occurrences)
+    {
+        frequencies.Add(character, times/(numberOfCharacters * 1.0D));
+    }
+
+    return frequencies;
+}
+
+
+Console.WriteLine("Contar espacios? s=si");
+var spaces = Console.ReadKey().Key == ConsoleKey.S;
+Console.WriteLine("\nIngrese el texto");
+var text = Console.ReadLine();
+if (text == null || string.IsNullOrWhiteSpace(text))
+{
+    Console.WriteLine("El texto no puede ser nulo o vacío");
+    return;
+}
+
+var symbolsOccurrence = SymbolsOccurrence(text.ToLower().Trim(), spaces);
+
+
+// Initial nodes are the leafs
+var nodes = symbolsOccurrence
+    .Select(symbolOccurrence => new Node() { 
+        Valor = new Dictionary<string, int>()
+        {
+            { symbolOccurrence.Key, symbolOccurrence.Value }
+        } 
+    }).OrderBy(node => node.NodeWeight()).ToList();
+
+Console.WriteLine(nodes.Count);
+
+
+var i = 0;
+while (true)
+{
+    if (nodes.Last().NodeWeight() == (spaces ? text.Length : text.Replace(" ", string.Empty).Length))
+    {
+        Console.WriteLine($"Peso total: {nodes.Last().NodeWeight()}");
+        break;
+    }
+    var nodeToAdd = new Node()
+    {
+        HijoIzquierdo = nodes[i],
+        HijoDerecho = nodes[i + 1]
+    };
+    nodes[i].Padre = nodeToAdd;
+    nodes[i + 1].Padre = nodeToAdd;
+    nodeToAdd.Valor = new Dictionary<string, int>()
+    {
+        {
+            nodes[i].Valor.First().Key + nodes[i + 1].Valor.First().Key,
+            nodes[i].Valor.First().Value + nodes[i + 1].Valor.First().Value
+        }
+    };
+    nodes.Add(nodeToAdd);
+        
+    i += 2;
+    
+    nodes = nodes.OrderBy(node => node.NodeWeight()).ToList();
+}
+
+
+nodes.Last().PrintRecursively();
